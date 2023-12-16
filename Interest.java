e71dbc37bdd9b47932f71f5e268b67375e2294f0
Philip <ajibolaphilip10@gmail.com>
2023-12-16 13:59:09 -0800

    chapter 5 exercise

---
+++ Interest.java
@@ -0,0 +1,26 @@
+public class Interest{
+	public static void main(String[] args){
+		double principal = 1000.0;
+		double rate = 0.05;
+	System.out.printf("%s%20s%n","year","Amount on Deposit");
+		for(int year = 1; year <=10;year++){
+	double amount = principal*Math.pow(1+rate,year);
+		rate +=0.01;
+			
+	System.out.printf("%4d%,20.2f%n", year, amount);
+
+	}	
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+}
